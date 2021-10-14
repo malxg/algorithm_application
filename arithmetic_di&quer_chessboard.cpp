72a7b4d8297c8ec6339f7019f68283291d39f08d
1305241494@qq.com <1305241494@qq.com>
2021-10-14 08:55:45 +0800

    arithmetic_divide_conquer_chessboard

---
+++ arithmetic_di&quer_chessboard.cpp
@@ -0,0 +1,18 @@
+//chessboard covering divide and conquer method
+#include <iostream>
+#define Maxnum 100001
+using namespace std;
+
+int order = 0;//The order of filling dominoes is not the execution order of code filling
+
+// 以左上角为坐标 零点，向下为 x的正轴，向右为 y的正轴 
+divide_conquer(int board[][],int x,int y,)
+{
+	
+}
+
+int main()
+{
+	int chessboard[Maxnum][Maxnum];
+	
+}
